6d4e54e8785ad964c45134a29815bc94852aadad
Joe <christowel@live.fr>
2020-04-29 10:45:34 -0400

    create react app and added files for homepage

--- src/index.js
+++ src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// <React.StrictMode> is a tool for detecting potential problems with an application
+// It's display nothing -- Strict mode checks are performed only during development.
+// They do not affect the version used in production .
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
